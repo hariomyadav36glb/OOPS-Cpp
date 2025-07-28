9d743989b70ff754dc2f4e41856b673452162141
Hari Om <hariomyadv36@gmail.com>
2025-07-28 16:42:13 +0530

    done

--- filehandling.cpp
+++ filehandling.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 int main(){
     // open the file
-    ofstream fout;
-    fout.open("zoom.txt");
-    // write kr sakta hu
-    fout<<"hello world";
-    fout.close(); /*release resources aquired*/
-}
+    // ofstream fout;
+    // fout.open("zoom.txt");
+    // // write kr sakta hu
+    // fout<<"hello world";
+    // fout.close(); /*release resources aquired*/
+
+    ifstream fin;
+    
+    fin.open("zoom.txt");
+
+    char c ;
+    c = fin.get();
+    while(!fin.eof())
+    {
+        cout<<c;
+        c = fin.get();
+    }
+      fin.close();
+
+} 
